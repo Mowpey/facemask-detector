a1677d75264cb17a31f0b26f8e3757d1a594d8c4
Dawntless <66586346+Mowpey@users.noreply.github.com>
2025-01-14 20:47:12 +0800

    Initial commit

---
+++ train_mask.py
@@ -0,0 +1,104 @@
+from tensorflow import keras
+from sklearn.preprocessing import LabelBinarizer
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import classification_report
+from imutils import paths
+import matplotlib.pyplot as plt
+import numpy as np
+import os
+
+INIT_LR = 1e-4
+EPOCS = 20
+BS = 32
+
+
+DIRECTORY = r"/Users/nijikya/Documents/AI Project/dataset"
+CATEGORIES = ["with_mask", "without_mask"]
+
+print("[INFO] loading images...")
+
+data = []
+labels = []
+
+for category in CATEGORIES:
+    path = os.path.join(DIRECTORY,category)
+    for img in os.listdir(path):
+        img_path = os.path.join(path, img)
+        image = keras.utils.load_img(img_path, target_size=(224, 224))
+        image = keras.utils.img_to_array(image)
+        image = keras.applications.mobilenet_v2.preprocess_input(image)
+
+        data.append(image)
+        labels.append(category)
+
+lb = LabelBinarizer()
+labels = lb.fit_transform(labels)
+labels = keras.utils.to_categorical(labels)
+
+data = np.array(data, dtype="float32")
+labels = np.array(labels)
+(trainX, testX, trainY, testY) = train_test_split(data, labels, test_size=0.20, stratify=labels, random_state=42)
+
+aug = keras.preprocessing.image.ImageDataGenerator(
+    rotation_range=20,
+    zoom_range=0.15,
+    width_shift_range=0.2,
+    height_shift_range=0.2,
+    shear_range=0.15,
+    horizontal_flip=True,
+    fill_mode="nearest"
+)
+
+baseModel = keras.applications.MobileNetV2(
+    weights="imagenet", include_top=False, input_tensor=keras.Input(shape=(224, 224, 3))
+)
+
+
+headModel = baseModel.output
+headModel = keras.layers.AveragePooling2D(pool_size=(7, 7))(headModel)
+headModel = keras.layers.Flatten(name="flatten")(headModel)
+headModel = keras.layers.Dense(128, activation="relu")(headModel)
+headModel = keras.layers.Dropout(0.5)(headModel)
+headModel = keras.layers.Dense(2, activation="softmax")(headModel)
+
+model = keras.Model(inputs=baseModel.input, outputs=headModel)
+
+for layer in baseModel.layers:
+    layer.trainable = False
+
+print("[INFO] compiling model...")
+opt = keras.optimizers.Adam(learning_rate=INIT_LR)
+model.compile(loss="binary_crossentropy", optimizer=opt, metrics=["accuracy"])
+
+print("[INFO] training head...")
+H = model.fit(
+    aug.flow(trainX, trainY, batch_size=BS),
+    steps_per_epoch=len(trainX) // BS,
+    validation_data=(testX, testY),
+    validation_steps=len(testX) // BS,
+    epochs=EPOCS
+)
+
+print("[INFO] evaluating network...")
+predIdxs = model.predict(testX, batch_size=BS)
+
+
+predIdxs = np.argmax(predIdxs, axis=1)
+
+print(classification_report(testY.argmax(axis=1), predIdxs, target_names=lb.classes_))
+
+print("[INFO] saving mask detector model...")
+model.save("mask_detector.h5")
+
+N=EPOCS
+plt.style.use("ggplot")
+plt.figure()
+plt.plot(np.arange(0, N), H.history["loss"], label="train_loss")
+plt.plot(np.arange(0, N), H.history["val_loss"], label="val_loss")
+plt.plot(np.arange(0, N), H.history["accuracy"], label="train_acc")
+plt.plot(np.arange(0, N), H.history["val_accuracy"], label="val_acc")
+plt.title("Training Loss and Accuracy")
+plt.xlabel("Epoch #")
+plt.ylabel("Loss/Accuracy")
+plt.legend(loc="lower left")
+plt.savefig("plot.png")
